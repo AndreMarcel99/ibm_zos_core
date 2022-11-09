19aae48fc2fe014c4a345b34c650bf56a4e43e98
Oscar Fernando Flores Garcia <fernandofloresdev@gmail.com>
2022-11-09 14:08:24 -0600

    Started with zos_archive dev

---
+++ plugins/modules/zos_archive.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) IBM Corporation 2020, 2022
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: zos_archive
+version_added: "1.6.0"
+author:
+  - Oscar Fernando Flores Garcia (@fernandofloresg)
+short_description: Archive a dataset on z/OS.
+
+
+
+description:
+  - Creates or extends an archive.
+  - The source and archive are on the remote host,
+    and the archive is not copied to the local host.
+
+options:
+  path:
+    description:
+      - Remote absolute path, glob, or list of paths or globs for the file or files to compress or archive.
+    type: list
+    required: true
+    elements: path
+  format:
+    description:
+      - The type of compression to use.
+    type: str
+    choices: [ bz2, gz, tar, zip ]
+    default: gz
+  dest:
+    description: The file name of the destination archive.
+    required: false
+    type: str
+  exclude_path:
+    description:
+        - Remote absolute path, glob, or list of paths or globs for the file or files to exclude
+           from path list and glob expansion.
+    required: false
+    type: list
+    elements: path
+'''
+
+EXAMPLES = r'''
+# Pass in a message
+- name: Test with a message
+  my_namespace.my_collection.my_test:
+    name: hello world
+
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  my_namespace.my_collection.my_test:
+    name: hello world
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  my_namespace.my_collection.my_test:
+    name: fail me
+'''
+
+RETURN = r'''
+state:
+    description:
+        The state of the input C(path).
+    type: str
+    returned: always
+dest_state:
+    description:
+      - The state of the I(dest) file.
+      - C(absent) when the file does not exist.
+      - C(archive) when the file is an archive.
+      - C(compress) when the file is compressed, but not an archive.
+      - C(incomplete) when the file is an archive, but some files under I(path) were not found.
+    type: str
+    returned: success
+    version_added: 3.4.0
+missing:
+    description: Any files that were missing from the source.
+    type: list
+    returned: success
+archived:
+    description: Any files that were compressed or added to the archive.
+    type: list
+    returned: success
+arcroot:
+    description: The archive root.
+    type: str
+    returned: always
+expanded_paths:
+    description: The list of matching paths from paths argument.
+    type: list
+    returned: always
+expanded_exclude_paths:
+    description: The list of matching exclude paths from the exclude_path argument.
+    type: list
+    returned: always
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.ibm.ibm_zos_core.plugins.module_utils import (
+    better_arg_parser)
+
+def run_module():
+    module = AnsibleModule(
+        argument_spec=dict(
+            path=dict(type='list', elements='path', required=True),
+            format=dict(type='str', default='gz', choices=['bz2', 'gz', 'tar', 'zip']),
+            dest=dict(type='str',),
+            exclude_path=dict(type='list', elements='path', default=[]),
+        ),
+        supports_check_mode=True,
+    )
+
+    arg_defs = dict(
+        path=dict(type='list', elements='path', required=True),
+        format=dict(type='str', default='gz', choices=['bz2', 'gz', 'tar', 'zip']),
+        dest=dict(type='str', required=True),
+        exclude_path=dict(type='list', elements='path', default=[]),
+    )
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    try:
+        parser = better_arg_parser.BetterArgParser(arg_defs)
+        parsed_args = parser.parse_args(module.params)
+    except ValueError as err:
+        module.fail_json(msg="Parameter verification failed", stderr=str(err))
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
