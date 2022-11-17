8a8eac3f78ff3a1671ac090abfe3221408e0f067
Oscar Fernando Flores Garcia <fernandofloresdev@gmail.com>
2022-11-17 13:40:16 -0600

    added extra params and docs

--- plugins/modules/zos_archive.py
+++ plugins/modules/zos_archive.py
@@ -37,23 +37,74 @@ options:
     type: list
     required: true
     elements: path
+    alias: src
   format:
     description:
       - The type of compression to use.
     type: str
     choices: [ bz2, gz, tar, zip ]
     default: gz
+    required: false
   dest:
     description: The file name of the destination archive.
-    required: false
     type: str
+    required: false
   exclude_path:
     description:
         - Remote absolute path, glob, or list of paths or globs for the file or files to exclude
            from path list and glob expansion.
-    required: false
     type: list
+    required: false
     elements: path
+  force_archive:
+    description:
+      - Allows you to force the module to treat this as an archive even if only a single file is specified.
+      - By default when a single file is specified it is compressed only (not archived).
+    type: bool
+    required: false
+    default: false
+  group:
+    description:
+      - Name of the group that should own the filesystem object, as would be fed to chown.
+      - When left unspecified, it uses the current group of the current user unless you are root,
+        in which case it can preserve the previous ownership.
+    type: str
+    required: false
+  mode:
+    description:
+      - The permissions the resulting filesystem object should have.
+    type: str
+    required: false
+  owner:
+    description:
+      - Name of the user that should own the filesystem object, as would be fed to chown.
+      - When left unspecified, it uses the current user unless you are root, in which case it can preserve the previous ownership.
+    type: str
+    required: false
+  remove:
+    description:
+      - Remove any added source files and trees after adding to archive.
+    type: bool
+    required: false
+    default: false
+  replace_dest:
+    description:
+      - Replace the existing archive C(dest).
+    type: bool
+    default: false
+    required: false
+    alias: force
+  list:
+    description:
+      - List the names of the archive contents
+    type: bool
+    default: false
+    required: false
+  tmp_hlq:
+    description:
+      - High Level Qualifier used for temporary datasets.
+    type: str
+    required: false
 '''
 
 EXAMPLES = r'''
@@ -116,22 +167,43 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.ibm.ibm_zos_core.plugins.module_utils import (
     better_arg_parser)
 
+
 def run_module():
     module = AnsibleModule(
         argument_spec=dict(
-            path=dict(type='list', elements='path', required=True),
-            format=dict(type='str', default='gz', choices=['bz2', 'gz', 'tar', 'zip']),
-            dest=dict(type='str',),
+            path=dict(type='list', elements='path', required=True, alias='src'),
+            dest=dict(type='str'),
             exclude_path=dict(type='list', elements='path', default=[]),
+            # Q1 I think we should use force in here instead of down, and change that one to replace.
+            force_archive=dict(type='bool', default=False),
+            format=dict(type='str', default='gz', choices=['bz2', 'gz', 'tar', 'zip']),
+            group=dict(type='str', default=''),
+            mode=dict(type='str', default=''),
+            owner=dict(type='str', default=''),
+            remove=dict(type='bool', default=False),
+            # Q1 I think this parameter name should be replace.
+            replace_dest=dict(type='bool', default=False, alias='force'),
+            list=dict(type='bool', default=False),
+            tmp_hlq=dict(type='str', default=''),
         ),
         supports_check_mode=True,
     )
 
     arg_defs = dict(
-        path=dict(type='list', elements='path', required=True),
-        format=dict(type='str', default='gz', choices=['bz2', 'gz', 'tar', 'zip']),
-        dest=dict(type='str', required=True),
+        path=dict(type='list', elements='path', required=True, alias='src'),
+        dest=dict(type='str', required=False),
         exclude_path=dict(type='list', elements='path', default=[]),
+        # Q1 I think we should use force in here instead of down, and change that one to replace.
+        force_archive=dict(type='bool', default=False),
+        format=dict(type='str', default='gz', choices=['bz2', 'gz', 'tar', 'zip']),
+        group=dict(type='str', default=''),
+        mode=dict(type='str', default=''),
+        owner=dict(type='str', default=''),
+        remove=dict(type='bool', default=False),
+        # Q1 I think this parameter name should be replace.
+        replace_dest=dict(type='bool', default=False, alias='force'),
+        list=dict(type='bool', default=False),
+        tmp_hlq=dict(type='qualifier_or_empty', default=''),
     )
     # seed the result dict in the object
     # we primarily care about changed and state
